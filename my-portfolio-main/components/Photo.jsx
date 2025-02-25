c9ed671a60e6c8cc9ba8c620e2dd26848979a4be
Aryan Bhatia <151797123+Adonalsiun@users.noreply.github.com>
2025-02-24 22:12:49 -0500

    Update UI

--- my-portfolio-main/components/Photo.jsx
+++ my-portfolio-main/components/Photo.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import profilepic from "../public/assets/profile.png";
+import profilepic from "../public/assets/lonrobl.png";
 
 const Photo = () => {
   return (
--- my-portfolio-main/tailwind.config.js
+++ my-portfolio-main/tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
       colors: {
         primary: '#1c1c22',
         accent: {
-          DEFAULT: '#00ff99',
+          DEFAULT: '#a3e9fb',
           hover: '#00e187',
         }
       },
